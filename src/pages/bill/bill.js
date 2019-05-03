c79276c3f55442060907717afe85829be86e7e56
wangnengjie <751614701@qq.com>
2019-05-03 22:12:35 +0800

    fix change in bill

--- src/pages/bill/bill.js
+++ src/pages/bill/bill.js
@@ -17,28 +17,28 @@ class Bill extends Component {
 
   initialize(type, page, ledgerId) {
     const uid = Taro.getStorageSync("uid");
-    if (type === "add") {   //加一笔
+    if (type === "add") {
+      //加一笔
       events.on("addOne", this.handleAddOne);
       // 从主页进来加一笔
       if (page === "index") {
         console.log(globalData);
-        var { members, ledgerName, use } = globalData;
+        var { categories, ledgerName, users } = globalData.ledger;
       } else {
-      // 从账单详情页进来加一笔
-        var { members, ledgerName, use } = tempLedgerData;
+        // 从账单详情页进来加一笔
+        var { categories, ledgerName, users } = tempLedgerData;
       }
 
-      members = JSON.parse(JSON.stringify(members));
-      console.log(members);
-      for (let i = 0; i < members.length; i++) {
-        members[i].selected = true;
+      users = JSON.parse(JSON.stringify(users));
+      for (let i = 0; i < users.length; i++) {
+        users[i].selected = true;
       }
 
       //默认记账人为付款人
-      for (let i = 0; i < members.length; i++) {
-        if (members[i].uid === uid) {
-          let user = members.splice(i, 1);
-          members.unshift(user[0]);
+      for (let i = 0; i < users.length; i++) {
+        if (users[i].UserId === uid) {
+          let user = users.splice(i, 1);
+          users.unshift(user[0]);
           break;
         }
       }
@@ -48,15 +48,15 @@ class Bill extends Component {
         page,
         ledgerId,
         ledgerName,
-        use,
-        members
+        categories,
+        users
       });
     } else if (type === "fix") {
       if (page === "index") {
         // 主页修改明细
         this.setState({
           ...tempBillData,
-          ledgerUse: globalData.use,
+          categories: globalData.ledger.categories,
           type,
           page
         });
@@ -64,7 +64,7 @@ class Bill extends Component {
         // 详情页修改明细
         this.setState({
           ...tempBillData,
-          ledgerUse: tempLedgerData.use,
+          categories: tempLedgerData.categories,
           type,
           page
         });
